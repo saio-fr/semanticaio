21ae94b0b097c78abe0e640a5a9a2439f2c55f5e
AlexisTerrat <alexisterrat@gmail.com>
2015-11-23 19:03:28 +0100

    src main controller v1 & maj doc

--- src/controller/main.js
+++ src/controller/main.js
@@ -5,6 +5,7 @@ var events = require('events');
 var semanticConfig = require('config/semantic.json');
 
 var Controller = function(container, options) {
+  var that = this;
   this.ws = container.use('ws', Wsocket, {
     url: 'ws://crossbar:8080',
     realm: 'semanticaio'
@@ -13,6 +14,22 @@ var Controller = function(container, options) {
   this.appState = 'new';
   this.trainerState = 'new';
   this.eventHandler = new events.EventEmitter();
+
+  this.eventHandler.on('encoderTrainerStarted', function() {
+    that.trainerState = 'training';
+    return that.ws.publish('semanticaio.trainer.started', [], {})
+    .tap(function() {
+      console.log('[emit] semanticaio.trainer.started');
+    });
+  });
+
+  this.eventHandler.on('encoderTrainerStopped', function() {
+    that.trainerState = 'ready';
+    return that.ws.publish('semanticaio.trainer.stopped', [], {})
+    .tap(function() {
+      console.log('[emit] semanticaio.trainer.stopped');
+    });
+  });
 };
 
 Controller.prototype.start = function() {
@@ -64,15 +81,12 @@ Controller.prototype.getTrainerState = function() {
 Controller.prototype.analyze = function(args, kwargs) {
   console.log('[call] semanticaio.analyze: ' + kwargs.question);
   var that = this;
-  var result = {
-    tags: {},
-    classes: {},
-    match: { question: '', distance: 1 }
-  };
   if (this.appState !== 'ready') {
     console.log('[error] semanticaio.analyze: app not ready');
-    return result;
+    return;
   }
+
+  var result = {};
   return this.ws.call('semanticaio.encoder.encode', [] {
     question: kwargs.question,
     decode: true
@@ -117,6 +131,7 @@ Controller.prototype.loadApp = function() {
     console.log('[error] semanticaio.app.load: still committing');
     return;
   }
+
   this.appState = 'loading';
   return this.ws.publish('semanticaio.app.state.loading', [], {})
   .then(function() {
@@ -132,10 +147,10 @@ Controller.prototype.loadApp = function() {
     return when.all(pendingPromises);
   })
   .then(function() {
+    that.appState = 'ready';
     return that.ws.publish('semanticaio.app.state.loaded', [], {});
   })
   .tap(function() {
-    that.appState = 'ready';
     console.log('[emit] semanticaio.app.state.loaded');
   });
 };
@@ -147,6 +162,7 @@ Controller.prototype.loadTrainer = function() {
     console.log('[error] semanticaio.trainer.load: invalid state: ' + this.trainerState);
     return;
   }
+
   this.trainerState = 'loading';
   return this.ws.publish('semanticaio.trainer.state.loading', [], {})
   .then(function() {
@@ -161,6 +177,7 @@ Controller.prototype.loadTrainer = function() {
     return when.all(pendingPromises);
   })
   .then(function() {
+    that.trainerState = 'ready';
     return that.ws.publish('semanticaio.trainer.state.loaded', [], {});
   })
   .tap(function() {
@@ -169,36 +186,158 @@ Controller.prototype.loadTrainer = function() {
 };
 
 Controller.prototype.startTrainer = function() {
+  var that = this;
+  console.log('[event received] semanticaio.trainer.start');
+  if (this.trainerState !== 'ready') {
+    console.log('[error] semanticaio.trainer.start: invalid state: ' + this.trainerState);
+    return;
+  }
 
+  this.trainerState = 'starting';
+  return this.ws.publish('semanticaio.trainer.state.starting', [], {})
+  .then(function() {
+    console.log('[emit] semanticaio.trainer.state.starting');
+    return that.ws.publish('semanticaio.encoder.trainer.start', [], {});
+  });
 };
 
 Controller.prototype.stopTrainer = function() {
+  var that = this;
+  console.log('[event received] semanticaio.trainer.stop');
+  if (this.trainerState !== 'training') {
+    console.log('[error] semanticaio.trainer.stop: invalid state: ' + this.trainerState);
+    return;
+  }
 
+  this.trainerState = 'stopping';
+  return this.ws.publish('semanticaio.trainer.state.stopping', [], {})
+  .then(function() {
+    console.log('[emit] semanticaio.trainer.state.stopping');
+    return that.ws.publish('semanticaio.encoder.trainer.stop', [], {});
+  });
 };
 
 Controller.prototype.commit = function() {
-  // change state
-  // save encoder
-  // load encoder
-  // re-encode db
-  // train tagger & train classifier
-  // load tagger, trainer, matcher
-  // fire committed
+  var that = this;
+  console.log('[event received] semanticaio.trainer.commit');
+  if (this.appState === 'loading') {
+    console.log('[error] semanticaio.trainer.commit: invalid app state: loading');
+    return;
+  }
+  if (this.trainerState !== 'ready') {
+    console.log('[error] semanticaio.trainer.commit: invalid trainer state: ' + this.trainerState);
+    return;
+  }
+
+  this.trainerState = 'committing';
+  this.appState = 'loading';
+  return this.ws.publish('semanticaio.trainer.state.committing', [], {})
+  .tap(function() {
+    console.log('[emit] semanticaio.trainer.state.committing');
+  })
+  .then(function() {
+    return that.ws.publish('semanticaio.app.state.loading', [], {});
+  })
+  .tap(function() {
+    console.log('[emit] semanticaio.app.state.loading');
+  })
+  .then(function() {
+    return that.ws.call('semanticaio.encoder.trainer.save', [], {});
+  })
+  .then(function() {
+    return that.ws.call('semanticaio.encoder.load', [], {});
+  })
+  .then(function() {
+    return that.ws.call('semanticaio.db.all.get', [], { correctOnly: true });
+  })
+  .then(function(getResult) {
+    var ids = _.map(getResult.questions, function(question) {
+      return question.id;
+    });
+    var sentences = _.map(getResult.questions, function(question) {
+      return question.sentence;
+    });
+    return that.ws.call('semanticaio.encoder.encode', [], {
+      questions: sentences,
+      decode: false
+    })
+    .then(function(encodeResult) {
+      var pendingUpdates = _.map(ids, function(id, index) {
+        return that.ws.call('semanticaio.db.set', [], {
+          id: id,
+          representation: encodeResult.encoded[i]
+        });
+      });
+      return when.all(pendingUpdates);
+    });
+  })
+  .then(function() {
+    return when.promise(function(resolve) {
+      var taggerTrained = false;
+      var classifierTrained = false;
+      that.eventHandler.once('taggerTrained', function() {
+        taggerTrained = true;
+        if (classifierTrained) {
+          resolve();
+        }
+      });
+      that.eventHandler.once('classifierTrained', function() {
+        classifierTrained = true;
+        if (taggerTrained) {
+          resolve();
+        }
+      });
+      that.ws.publish('semanticaio.tagger.trainer.train', [], {});
+      that.ws.publish('semanticaio.classifier.trainer.train', [], {});
+    });
+  })
+  .then(function() {
+    var pendingSaves = [
+      that.ws.call('semanticaio.tagger.trainer.save'),
+      that.ws.call('semanticaio.classifier.trainer.save')
+    ];
+    return when.all(pendingSaves);
+  })
+  .then(function() {
+    var pendingLoads = [
+      that.ws.call('semanticaio.tagger.load'),
+      that.ws.call('semanticaio.classifier.load'),
+      that.ws.call('semanticaio.matcher.load')
+    ];
+    return when.all(pendingLoads);
+  })
+  .then(function() {
+    that.appState = 'ready';
+    that.trainerState = 'ready';
+    var pendingPubs = [
+      that.ws.publish('semanticaio.app.state.loaded'),
+      that.ws.publish('semanticaio.trainer.state.committed')
+    ];
+    return when.all(pendingPubs);
+  })
+  .tap(function() {
+    console.log('[emit] semanticaio.app.state.loaded');
+    console.log('[emit] semanticaio.trainer.state.committed');
+  });
 };
 
 Controller.prototype._onEncoderTrainerStarted = function() {
+  console.log('[internal event received] semanticaio.encoder.trainer.started');
   this.eventHandler.emit('encoderTrainerStarted');
 };
 
 Controller.prototype._onEncoderTrainerStopped = function() {
+  console.log('[internal event received] semanticaio.encoder.trainer.stopped');
   this.eventHandler.emit('encoderTrainerStopped');
 };
 
 Controller.prototype._onTaggerTrained = function() {
+  console.log('[internal event received] semanticaio.tagger.trainer.trained');
   this.eventHandler.emit('taggerTrained');
 };
 
 Controller.prototype._onClassifierTrained = function() {
+  console.log('[internal event received] semanticaio.classifier.trainer.trained');
   this.eventHandler.emit('classifierTrained');
 };
 
